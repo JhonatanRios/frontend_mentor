2c35a97547867f64817caf656edd81d4e7ed089e
JhonatanRios <jhoto1234@gmail.com>
2023-04-13 14:44:01 -0500

    Ajustar filtro de proyectos

--- docs/js/script.js
+++ docs/js/script.js
@@ -34,6 +34,9 @@ function createFilters(data, attributeName) {
   inputAll.type = 'radio';
   inputAll.name = attributeName;
   inputAll.value = 'all';
+  inputAll.checked = true;
+  inputAll.id = 'all';
+  labelAll.htmlFor = 'all';
   labelAll.textContent = 'All';
   radioGroup.appendChild(inputAll);
   radioGroup.appendChild(labelAll);
@@ -42,7 +45,8 @@ function createFilters(data, attributeName) {
     const input = document.createElement('input');
     input.type = 'radio';
     input.name = attributeName;
-    input.value = value;
+    input.id = value;
+    label.htmlFor = value;
     label.textContent = value;
     radioGroup.appendChild(input);
     radioGroup.appendChild(label);
@@ -69,55 +73,90 @@ async function initFliterProyects() {
 }
 
 /*
-  Crear tarjetas debido al JSON
+  Decide que trajetas cargar dependiendo que input esta checked
 */
-async function createCards() {
+async function filterCards() {
   jsonData = await readJson('js/proyects.json');
   
+  // Agregar tarjetas al cargar la página
   for (const item of jsonData) {
-    const card = document.createElement('a');
-    card.classList.add('card');
-    card.href = item.url;
-
-    const info = document.createElement('div');
-    info.classList.add('info');
-
-    const img = document.createElement('img');
-    img.src = item.thumbnail;
-    img.alt = item.nomProyecto;
-
-    const title = document.createElement('h2');
-    title.textContent = item.nomProyecto;
-
-    const level = document.createElement('p');
-    level.textContent = item.level;
-    if (item.level === "newbie") {
-      level.style.color = "#6abecd";
-    } else if (item.level === "junior") {
-      level.style.color = "#aad742";
-    } else if (item.level === "intermediate") {
-      level.style.color = "#f1b604";
-    } else if (item.level === "advanced") {
-      level.style.color = "#f48925";
-    } else if (item.level === "guru") {
-      level.style.color = "#ed2c49";
-    }
-    console.log(level.textContent);
-
-    const builtWith = document.createElement('p');
-    builtWith.textContent = `${item.built.join(', ')}`;
-
-    card.appendChild(img);
-    info.appendChild(title);
-    info.appendChild(builtWith);
-    info.appendChild(level);
-    card.appendChild(info);
+    const card = createCards(item);
     cards.appendChild(card);
   }
+
+  // Agregar event listeners a los botones de radio
+  const radioButtons = document.querySelectorAll('input[name="level"]');
+  radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+      const selectedLevel = radio.id;
+      console.log(selectedLevel);
+      cards.innerHTML = ''; // clear previous cards
+      
+      if (selectedLevel === 'all') {
+        for (const item of jsonData) {
+          const card = createCards(item);
+          cards.appendChild(card);
+        }
+      } else {
+        const filteredData = jsonData.filter(item => item.level === selectedLevel);
+        for (const item of filteredData) {
+          const card = createCards(item);
+          cards.appendChild(card);
+        }
+      }
+    });
+  });
+}
+
+/*
+  Crear tarjetas
+*/
+function createCards(item) {
+  const card = document.createElement('a');
+  card.classList.add('card');
+  card.href = item.url;
+
+  const info = document.createElement('div');
+  info.classList.add('info');
+
+  const img = document.createElement('img');
+  img.src = item.thumbnail;
+  img.alt = item.nomProyecto;
+
+  const title = document.createElement('h2');
+  title.textContent = item.nomProyecto;
+
+  const level = document.createElement('p');
+  level.textContent = item.level;
+  if (item.level === "newbie") {
+    level.style.color = "#6abecd";
+  } else if (item.level === "junior") {
+    level.style.color = "#aad742";
+  } else if (item.level === "intermediate") {
+    level.style.color = "#f1b604";
+  } else if (item.level === "advanced") {
+    level.style.color = "#f48925";
+  } else if (item.level === "guru") {
+    level.style.color = "#ed2c49";
+  }
+
+  const builtWith = document.createElement('p');
+  builtWith.textContent = `${item.built.join(', ')}`;
+
+  card.appendChild(img);
+  info.appendChild(title);
+  info.appendChild(builtWith);
+  info.appendChild(level);
+  card.appendChild(info);
+
+  return card;
 }
 
 
 
 
+
+
+
 initFliterProyects();
-createCards();
+filterCards();
