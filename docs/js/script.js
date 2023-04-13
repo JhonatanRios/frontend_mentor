a862ee47edd49ec4f77288768a7f4169273c926e
JhonatanRios <jhoto1234@gmail.com>
2023-04-12 22:40:32 -0500

    Conectar proyectos con pagina frontend mentor

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,46 +1,111 @@
 const filtroLevels = document.getElementById('filtros');
+const cards = document.getElementById('cards');
+let jsonData = '';
 
-// Create the items div only once
-if (!document.getElementById('items')) {
-  const div = document.createElement('div');
-  div.id = 'items';
-  filtroLevels.insertBefore(div, filtroLevels.firstChild);
+/*
+  try: Lee archivo que se le pase por parametro y retorna un objeto
+  catch: si no se devuelve la promesa (await) lanza un error
+*/
+async function readJson(filename) {
+  try {
+    const response = await fetch(filename);
+    jsonData = await response.json();
+    //console.log(jsonData);
+    return jsonData;
+  } catch (error) {
+    console.error(error);
+    throw new Error('No se pudo leer el archivo json.');
+  }
 }
 
-function generarInputsRadioLevels() {
-  fetch('./js/filtros.json')
-  .then(response => response.json())
-  .then(data => {
-    // Clear any existing content from the items div before adding new inputs
-    const itemsDiv = document.getElementById('items');
-    itemsDiv.innerHTML = '';
-
-    for (let i = 0; i < data.length; i++) {
-      const input = document.createElement('input');
-      input.type = 'radio';
-      input.id = data[i].id;
-      input.name = 'levels';
-      input.value = data[i].id;
-      
-      const label = document.createElement('label');
-      label.htmlFor = data[i].id;
-      label.textContent = data[i].nombre;
-      
-      // Check the first input element created
-      if (i === 0) {
-        input.checked = true;
-      }
-      
-      itemsDiv.appendChild(input);
-      itemsDiv.appendChild(label);
-    }
-  })
-  .catch(error => {
-    // Display an error message to the user if the fetch request fails
-    const itemsDiv = document.getElementById('items');
-    itemsDiv.innerHTML = '<p>Los filtros no se pueden cargar debido a un error.</p>';
-    console.error('Error:', error);
+/*
+  Resive por parametro una objeto + unstring que seta el nombre del atributo a selecccionar
+    - Crea contenedor con id para los estilos
+    - Crea input + label para una opcion general
+    - Recorre el objeto buscando el atributo ambos entregados por parametro y va generando input + label cada que encuentra 1 diferente
+    - Retorna un div+id que contiene los inputs + labels
+*/
+function createFilters(data, attributeName) {
+  const attributeValues = new Set(data.map((item) => item[attributeName]));
+  const radioGroup = document.createElement('div');
+  radioGroup.id = 'items';
+  const labelAll = document.createElement('label');
+  const inputAll = document.createElement('input');
+  inputAll.type = 'radio';
+  inputAll.name = attributeName;
+  inputAll.value = 'all';
+  labelAll.textContent = 'All';
+  radioGroup.appendChild(inputAll);
+  radioGroup.appendChild(labelAll);
+  attributeValues.forEach((value) => {
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    input.type = 'radio';
+    input.name = attributeName;
+    input.value = value;
+    label.textContent = value;
+    radioGroup.appendChild(input);
+    radioGroup.appendChild(label);
   });
+  return radioGroup;
 }
 
-generarInputsRadioLevels();
+/*
+  Usa las 2 funciones readJson(), createFilters()
+*/
+async function initFliterProyects() {
+  try {
+    jsonData = await readJson('js/proyects.json');
+    if (jsonData) {
+      filtroLevels.innerHTML = '';
+      const radioGroup = createFilters(jsonData, 'level');
+      filtroLevels.appendChild(radioGroup);
+    } else {
+      filtroLevels.textContent = 'No se pudo leer el archivo json.';
+    }
+  } catch (error) {
+    filtroLevels.textContent = error.message;
+  }
+}
+
+/*
+  Crear tarjetas debido al JSON
+*/
+async function createCards() {
+  jsonData = await readJson('js/proyects.json');
+  
+  for (const item of jsonData) {
+    const card = document.createElement('a');
+    card.classList.add('card');
+    card.href = item.url;
+
+    const info = document.createElement('div');
+    info.classList.add('info');
+
+    const img = document.createElement('img');
+    img.src = item.thumbnail;
+    img.alt = item.nomProyecto;
+
+    const title = document.createElement('h2');
+    title.textContent = item.nomProyecto;
+
+    const level = document.createElement('p');
+    level.textContent = item.level;
+
+    const builtWith = document.createElement('p');
+    builtWith.textContent = `${item.built.join(', ')}`;
+
+    card.appendChild(img);
+    info.appendChild(title);
+    info.appendChild(builtWith);
+    info.appendChild(level);
+    card.appendChild(info);
+    cards.appendChild(card);
+  }
+}
+
+
+
+
+initFliterProyects();
+createCards();
