c3c50a121386f16499811c425e1919e6b78e4062
JhonatanRios <jhoto1234@gmail.com>
2023-04-15 17:07:41 -0500

    nuevo proyecto

---
+++ docs/newbie/interactive-rating-component-main/js/app.js
@@ -0,0 +1,15 @@
+const how = document.getElementById('cardHow');
+const thanks = document.getElementById('cardThanks');
+const rating = document.getElementById('ratings');
+
+function califica(event) {
+  event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+  const selectOption = rating.elements.rating.value; // Obtiene el valor seleccionado
+  how.style.display = "none";
+  thanks.style.display = "grid";
+  console.log(selectOption); // Muestra el valor seleccionado en la consola
+}
+
+rating.addEventListener('submit', califica);
+
+You selected <!-- Add rating here --> out of 5
