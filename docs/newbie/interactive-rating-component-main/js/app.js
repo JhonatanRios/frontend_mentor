be9a87e863ce845367f1db72890c30c353befd83
JhonatanRios <jhoto1234@gmail.com>
2023-04-15 17:45:07 -0500

    Agregando funcionalidad de cambo de card

--- docs/newbie/interactive-rating-component-main/js/app.js
+++ docs/newbie/interactive-rating-component-main/js/app.js
@@ -1,15 +1,18 @@
 const how = document.getElementById('cardHow');
 const thanks = document.getElementById('cardThanks');
 const rating = document.getElementById('ratings');
+const answer = document.getElementById('answer');
+const textAnswer = document.createElement('p');
 
 function califica(event) {
   event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
   const selectOption = rating.elements.rating.value; // Obtiene el valor seleccionado
   how.style.display = "none";
   thanks.style.display = "grid";
-  console.log(selectOption); // Muestra el valor seleccionado en la consola
+
+  textAnswer.textContent = `You selected ${selectOption} out of 5`;
+  answer.appendChild(textAnswer);
+  console.log(answer); // Muestra el valor seleccionado en la consola
 }
 
 rating.addEventListener('submit', califica);
-
-// You selected <!-- Add rating here --> out of 5
